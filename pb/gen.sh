aaab5dfd754688a491264b834e12f87f3822e908
shimingyah <shimingyah@gmail.com>
2019-06-26 09:38:23 +0800

    init code

--- pb/gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# You might need to go get -v github.com/gogo/protobuf/...
-
-protos=${GOPATH-$HOME/go}/src/github.com/shimingyah/wisckey/pb
-pushd $protos > /dev/null
-protoc --gofast_out=plugins=grpc:. -I=. pb.proto
