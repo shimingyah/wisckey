3016ad59454108397e3822275782f62312ab737d
shimingyah <shimingyah@gmail.com>
2019-06-27 09:21:51 +0800

    add wisckey options

---
+++ options.go
@@ -0,0 +1,75 @@
+package wisckey
+
+// Options are params for creating DB object.
+//
+// This package provides DefaultOptions which contains options that should
+// work for most applications. Consider using that as a starting point before
+// customizing it for your own needs.
+type Options struct {
+	// Directory to store the lsm data in. If it doesn't exist, Wisckey will
+	// try to create it for you.
+	LSMDir string
+
+	// File or device to store the value log in.
+	VLogPath string
+
+	// Sync all writes to disk. Setting this to false would achieve better
+	// performance, but may cause data to be lost.
+	Sync bool
+
+	// How many versions to keep per key.
+	NumVersions int
+
+	// Open the DB as read-only. With this set, multiple processes can
+	// open the same Wisckey DB. Note: if the DB being opened had crashed
+	// before and has vlog data to be replayed, ReadOnly will cause Open
+	// to fail with an appropriate message.
+	ReadOnly bool
+
+	// DB-specific logger which will override the global logger.
+	Logger Logger
+
+	// Each table (or file) is at most this size.
+	MaxTableSize int64
+
+	// Maximum number of levels of compaction.
+	MaxLevels int
+
+	// If value size >= this threshold, only store value offsets in lsm tree.
+	ValueThreshold int
+
+	// Maximum number of tables to keep in memory, before stalling.
+	NumMemtables int
+
+	// Maximum number of Level 0 tables before we start compacting.
+	NumLevelZeroTables int
+
+	// Maximum total size for L1.
+	LevelOneSize int64
+
+	// Number of compaction workers to run concurrently. Setting this to zero would stop compactions
+	// to happen within LSM tree. If set to zero, writes could block forever.
+	NumCompactors int
+
+	// max entries in batch
+	maxBatchCount int64
+
+	// max batch size in bytes
+	maxBatchSize int64
+}
+
+// DefaultOptions sets a list of recommended options for good performance.
+// Feel free to modify these to suit your needs.
+var DefaultOptions = Options{
+	Sync:               true,
+	NumVersions:        1,
+	ReadOnly:           false,
+	Logger:             nil,
+	MaxTableSize:       64 << 20,
+	MaxLevels:          7,
+	ValueThreshold:     512,
+	NumMemtables:       5,
+	NumLevelZeroTables: 5,
+	LevelOneSize:       256 << 20,
+	NumCompactors:      2,
+}
